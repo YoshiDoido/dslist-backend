57c2c2d72b8aac53ba2eb24e90d310f663af198f
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2023-11-14 16:40:40 -0300

    Added Game Seed

--- src/main/java/com/gabrielyoshino/dslist/entities/Game.java
+++ src/main/java/com/gabrielyoshino/dslist/entities/Game.java
@@ -14,20 +14,24 @@ public class Game {
     private String title;
     @Column(name = "game_year")
     private Integer year;
+    private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
 
     }
 
-    public Game(Long id, String title, Integer year, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre ,String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
+        this.genre = genre;
         this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
@@ -57,6 +61,14 @@ public class Game {
 
     public void setYear(Integer year) {
         this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
     public String getPlatforms() {
